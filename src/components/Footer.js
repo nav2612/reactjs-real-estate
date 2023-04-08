2e74b64d4205e7c11164e8d7b4931718bbf9b45c
nav2612 <navpreet51198@gmail.com>
2023-04-07 17:21:27 -0700

    #2*

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,7 +8,7 @@ const Footer = () => {
                     <div className="col-lg-3 col-md-6">
                         <i className="fas fa-home"></i>
                         <span className="footer-other-text d-block mt-3 mb-3">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua
+                            Let's find you a place of your dream!
                         </span>
                         <div className="footer-social">
                             <div className="footer-social-item"><i className="fab fa-facebook"></i></div>
