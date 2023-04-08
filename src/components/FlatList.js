7cdf711116c080005e499689938b879169643205
nav2612 <navpreet51198@gmail.com>
2023-04-07 17:43:17 -0700

    #3

--- src/components/FlatList.js
+++ src/components/FlatList.js
@@ -1,31 +1,107 @@
-
-import Title from "./Title"
-import FlatItem from "./FlatItem"
+import { useState } from "react";
+import Title from "./Title";
+import FlatItem from "./FlatItem";
 
 const FlatList = () => {
-    const title = {
-        text: "Explore the listings near you",
-        description: "Check out the properties"
-    }
-    return (
-        <section className="section-all-re">
-            <div className="container">
-                <Title title={title.text} description={title.description} />
-                <div className="row">
-                    <FlatItem slug="lorem-ipsum-1" />
-                    <FlatItem slug="lorem-ipsum-2" />
-                    <FlatItem slug="lorem-ipsum-3" />
-                    <FlatItem slug="lorem-ipsum-4" />
-                    <FlatItem slug="lorem-ipsum-5" />
-                    <FlatItem slug="lorem-ipsum-6" /> 
-                    <FlatItem slug="lorem-ipsum-7" />
-                    <FlatItem slug="lorem-ipsum-8" />
-                    <FlatItem slug="lorem-ipsum-9" />  
-                </div>
-            </div>
-        </section>
-    )
+  const title = {
+    text: "Explore the listings near you",
+    description: "Check out the properties",
+  };
+ 
+  const flats = [
+  {
+    id: 1,
+    slug: "lorem-ipsum-1",
+  },
+  {
+    id: 2,
+    slug: "lorem-ipsum-2",
+  },
+  {
+    id: 3,
+    slug: "lorem-ipsum-2",
+  },
+  {
+    id: 4,
+    slug: "lorem-ipsum-2",
+  },
+  {
+    id: 5,
+    slug: "lorem-ipsum-2",
+  },
+  {
+    id: 6,
+    slug: "lorem-ipsum-2",
+  },
+  {
+    id: 7,
+    slug: "lorem-ipsum-2",
+  },
+  {
+    id: 8,
+    slug: "lorem-ipsum-2",
+  },
+  {
+    id: 9,
+    slug: "lorem-ipsum-2",
+  },
+  {
+    id: 10,
+    slug: "lorem-ipsum-2",
+  },
+  {
+    id: 11,
+    slug: "lorem-ipsum-2",
+  },
+  {
+    id: 12,
+    slug: "lorem-ipsum-2",
+  },
+  {
+    id: 13,
+    slug: "lorem-ipsum-2",
+  }
+];
 
-}
+  const [currentPage, setCurrentPage] = useState(1);
+  const flatsPerPage = 9;
+  const indexOfLastFlat = currentPage * flatsPerPage;
+  const indexOfFirstFlat = indexOfLastFlat - flatsPerPage;
+  const flatsToShow = flats.slice(indexOfFirstFlat, indexOfLastFlat);
+
+  const totalPages = Math.ceil(flats.length / flatsPerPage);
+
+  const handleNextPage = () => {
+    setCurrentPage(currentPage + 1);
+  };
+
+  const handlePrevPage = () => {
+    setCurrentPage(currentPage - 1);
+  };
+
+  return (
+    <section className="section-all-re">
+      <div className="container">
+        <Title title={title.text} description={title.description} />
+        <div className="row">
+          {flatsToShow.map((flat) => (
+            <FlatItem key={flat.id} slug={flat.slug} />
+          ))}
+        </div>
+        <div className="pagination" style={{ display: "flex", justifyContent: "center" }}>
+          {currentPage > 1 && (
+            <button onClick={handlePrevPage}>Previous</button>
+          )}
+          <span>
+            Page {currentPage} of {totalPages}
+          </span>
+          {currentPage < totalPages && (
+            <button onClick={handleNextPage}>Next</button>
+          )}
+        </div>
+      </div>
+    </section>
+  );
+};
 
 export default FlatList;
